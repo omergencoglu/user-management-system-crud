a6779af48d4f0f1ef93e52fee464a15274185886
Omer Gencoglu <omersaitgencoglu@gmail.com>
2021-11-11 15:30:22 -0500

    Started creating ejs files in views

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 
@@ -10,8 +11,19 @@ const port = process.env.PORT || 8080;
 //log request
 app.use(morgan("tiny"));
 
+//parse request
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//set view engine
+app.set("view engine", "ejs");
+//app.set('views', path.resolve(__dirname, "views/ejs"))
+
+//load assets
+app.use(express.static(path.join(__dirname, "assets")));
+
 app.get("/", (req, res) => {
-  res.send("Crud Application");
+  res.render("index");
 });
 
 app.listen(port, () => {
