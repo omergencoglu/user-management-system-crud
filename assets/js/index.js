052a3aebb7d24a00597e5bce99afde990d26de40
Omer Gencoglu <omersaitgencoglu@gmail.com>
2021-11-18 15:26:48 -0500

    Added delete functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,3 +24,22 @@ $("#update_user").submit(function (event) {
   });
   window.location.replace("/");
 });
+
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    const id = $(this).attr("data-id");
+
+    const request = {
+      url: `http://localhost:3000/api/users/${id}`,
+      method: "DELETE",
+    };
+
+    if (confirm("Do you really want to delete this record?")) {
+      $.ajax(request).done(function (response) {
+        alert("User deleted successfully!");
+        location.reload();
+      });
+    }
+  });
+}
